54e3b7df70591be9cffd48712049a2d1d50dcb83
Mason Phillips <mjphillips2000@gmail.com>
2020-10-12 19:52:36 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,16 @@
-hello_t("Tim", "Tom", "Jim").each do |name|
+hello_t(["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end"Tim", "Tom", "Jim").each do |name|
   puts "#{name}"
 end
 
 # call your method here!
 
 
+["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
